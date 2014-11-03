0eeb171aaea1de6b5481f58e3028a835db5c2585
Damien Metzler <dmetzler@nuxeo.com>
2014-11-03 15:10:36 +0100

    NXIO-332 Add a way to show stacktrace with a SIGUSR1 signal

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"runtime/pprof"
+	"syscall"
 	"github.com/golang/glog"
 )
 
@@ -29,6 +33,8 @@ func main() {
 
 	c := parseConfig()
 
+	handleSignals()
+
 	resolver, error := getResolver(c)
 	if error != nil {
 		panic(error)
@@ -39,5 +45,24 @@ func main() {
 		p := NewProxy(c, resolver)
 		p.start()
 	}
+
+}
+
+
+func handleSignals() {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
+	signal.Notify(signals, os.Interrupt, syscall.SIGUSR1)
+	go func() {
+		sig := <-signals
+		switch sig {
+		case syscall.SIGTERM, syscall.SIGINT:
+			//Exit gracefully
+			glog.Info("Shutting down...")
+			os.Exit(0)
+		case syscall.SIGUSR1:
+			pprof.Lookup("goroutine").WriteTo(os.Stdout, 1)
+		}
+	}()
 
 }
